80c87cdf7c3bcc03da9f87d4145d1d25b7caeb1f
dshergill <diljotshergill@hotmail.com>
2017-02-12 23:29:05 -0700

    Commit for hand-in

--- A2/code.js
+++ A2/code.js
@@ -31,12 +31,19 @@ function getStats(txt) {
         }
         let maxLineLength = getMaxLineLength().longestLine;
 
+        function getListOfWords () {
+            let alphaNumericWords = txt.replace(/[^0-9a-z]/gi, ' ').toLowerCase();
+            let listOfWords = alphaNumericWords.trim().split(/\s+/);
+            return {
+                listOfWords
+            }
+        }
+
         function getAverageWordLength() {
-            let alphaNumericWords = txt.replace(/[^0-9a-z]/gi, ' ');
-            let words = alphaNumericWords.trim().split(/\s+/);
+            let words = getListOfWords().listOfWords;
             let sumOfWords = 0;
-            for (let i=0; i<words.length;i++) {
-                let lenOfWord = words[i].length;
+            for (let w in words) {
+                let lenOfWord = words[w].length;
                 sumOfWords += lenOfWord;
             }
             return {
@@ -45,17 +52,96 @@ function getStats(txt) {
         }
         let averageWordLength = (getAverageWordLength().sumOfWords)/nWords;
 
+        function getPalindromes() {
+            let words = getListOfWords().listOfWords;
+            let arrayOfPalindromes = [];
+            for (let w in words) {
+                if ((words[w] == words[w].split('').reverse().join('') && (words[w].length > 1))) {
+                    arrayOfPalindromes.push(words[w]);
+                }
+            }
+            return {
+                arrayOfPalindromes
+            }
+        }
+        let palindromes = getPalindromes().arrayOfPalindromes;
 
-    return {
+        function getLongestWords() {
+            let words = getListOfWords().listOfWords;
+            let arrayOfLongestWords = words.sort(function(a, b) {
+                return a.length - b.length ||
+                    a.localeCompare(b);
+                }).reverse();
+            let nonRepetitiveWords = arrayOfLongestWords.filter(function( item, index, inputArray ) {
+                return inputArray.indexOf(item) == index;
+            });
+            let tenLongestWords = nonRepetitiveWords.slice(0, 10);
+            return {
+                tenLongestWords
+            }
+        }
+
+        let longestWords = getLongestWords().tenLongestWords;
+
+
+        function getFrequentWords() {
+            let words = getListOfWords().listOfWords;
+            let wordCounts = {};
+            for(let word in words) {
+                wordCounts[words[word]] = (wordCounts[words[word]] || 0) + 1;
+            }
+            let sorted = [];
+            for (let w in wordCounts) {
+                sorted.push([w, wordCounts[w]])
+            }
+            sorted.sort(function (a, b) {
+                return a[1] - b[1]
+            }).reverse();
+            let tempArr1 = [];
+            let tempArr2 = [];
+            let tempArr3 = [];
+            let j = 0, k = 1;
+            for(let i = 0; i < sorted.length; i++)
+            {
+                tempArr1 = tempArr1.concat(sorted[i]);
+            }
+            for(let i = 0; i < (tempArr1.length)/2; i++)
+            {
+                tempArr2[i] = tempArr1[j];
+                j+=2;
+            }
+            for(let i = 0; i < (tempArr1.length)/2; i++)
+            {
+                tempArr3[i] = tempArr1[k];
+                k+=2;
+            }
+            let sortedArray = [];
+            for(let i = 0; i < tempArr2.length; i++)
+            {
+                sortedArray[i] = tempArr2[i] + "(" + tempArr3[i] + ")";
+            }
+            let topTenFreq = sortedArray.slice(0,10);
+
+            return {
+                topTenFreq
+            }
+        }
+
+        let mostFrequentWords = getFrequentWords().topTenFreq;
+
+
+
+        return {
         nChars,
         nWords,
         nLines,
         nNonEmptyLines,
         maxLineLength,
         averageWordLength,
-        palindromes: ["12321", "kayak", "mom"],
-        longestWords: ["xxxxxxxxx", "123444444"],
-        mostFrequentWords: ["hello(7)", "world(1)"]
+        palindromes,
+        longestWords,
+        mostFrequentWords,
+
     };
 }
 
